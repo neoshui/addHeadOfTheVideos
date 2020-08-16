e83f5518164741ad02f2f9e37949aaf411d3a9a8
EricShui <2426726839@qq.com>
2020-08-16 19:32:03 +0800

    add logo

--- create_img.py
+++ create_img.py
@@ -10,28 +10,42 @@ class Videos:
     def __init__(self):
         pass
 
-    def draw_img(self, new_img, text):
+    def draw_img(self, new_img, text, logo_text):
         text = str(text)
+        logo_text = str(logo_text)
         draw = ImageDraw.Draw(new_img)
         img_size = new_img.size
-        draw.line((0, 0) + img_size, fill=128)
-        draw.line((0, img_size[1], img_size[0], 0), fill=128)
-        font_size = 40
-        font = ImageFont.truetype('STHeitiMedium.ttc', font_size)
-        img_font_size = font.getsize(text)
-        while img_font_size[0] > img_size[0]:
-            font_size -= 5
-            font = ImageFont.truetype('STHeitiMedium.ttc', font_size)
-            img_font_size = font.getsize(text)
-        x = (img_size[0] - img_font_size[0]) / 2
-        y = (img_size[1] - img_font_size[1]) / 2
-        draw.text((x, y), text, font=font, fill=(25, 25, 25))
+        # draw.line((0, 0) + img_size, fill=128)
+        # draw.line((0, img_size[1], img_size[0], 0), fill=128)
+
+        title_font_size = 100
+        title_font = ImageFont.truetype('STHeitiMedium.ttc', title_font_size)
+        title_img_font_size = title_font.getsize(text)
+        while title_img_font_size[0] > img_size[0]:
+            title_font_size -= 5
+            title_font = ImageFont.truetype('STHeitiMedium.ttc', title_font_size)
+            title_img_font_size = title_font.getsize(text)
+
+        logo_font_size = 90
+        logo_font = ImageFont.truetype('STHeitiMedium.ttc', logo_font_size)
+        logo_img_font_size = logo_font.getsize(text)
+
+        title_x = (img_size[0] - title_img_font_size[0]) / 2
+        title_y = (img_size[1] - title_img_font_size[1]) / 2
+        logo_x = (img_size[0] - (logo_img_font_size[0]))
+        logo_y = (img_size[1] - (logo_img_font_size[1]))
+        print(img_size[0], img_size[1])
+        print(logo_img_font_size[0], logo_img_font_size[1])
+        print(logo_x, logo_y)
+
+        draw.text((title_x, title_y), text, font=title_font, fill=(25, 25, 25))
+        draw.text((logo_x, logo_y), logo_text, font=title_font, fill=(25, 25, 25))
         pass
 
-    def create_img(self, width, height, text='default', color=(100, 100, 100, 100)):
+    def create_img(self, width, height, text='title', logo_text='logo', color=(100, 100, 100, 100)):
         new_img = Image.new('RGBA', (int(width), int(height)), color)
-        self.draw_img(new_img, text)
-        new_img.save('f.png')
+        self.draw_img(new_img, text, logo_text)
+        new_img.save(f'{text}.png')
         pass
 
     def get_video_info(self, file):
@@ -44,17 +58,24 @@ class Videos:
             file_name = file.split('.')[0]
 
             print(fps, width, height, file_name)
+            return [fps, width, height, file_name]
+        else:
+            return []
 
     def gen_file(self, path):
         files_list = os.listdir(path)
         file_list = []
         for file in files_list:
-            if not os.path.isdir(path + '/' + file):
+            if not os.path.isdir(file):
                 file_list.append(file)
-                self.get_video_info(file)
+        return file_list
 
 
 if __name__ == '__main__':
     v = Videos()
     # v.create_img(100, 200, text='这是测试')
-    v.gen_file('./')
+    files = v.gen_file('./')
+    for file in files:
+        video_info = v.get_video_info(file)
+        if video_info != []:
+            v.create_img(video_info[1], video_info[2], video_info[3], 'logo')
