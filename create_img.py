3f6bead126c1f62427a18c14ff6ac32d828add6f
EricShui <2426726839@qq.com>
2020-08-22 21:55:48 +0800

    final test

--- create_img.py
+++ create_img.py
@@ -11,6 +11,92 @@ class Videos:
     def __init__(self):
         pass
 
+    def gen_file(self, path):
+        """
+        读取文件
+        :param path:
+        :return:
+        """
+        files_list = os.listdir(path)
+        file_list = []
+        for file in files_list:
+            if not os.path.isdir(file):
+                file_list.append(file)
+        return file_list
+
+    def walk_dir(self, root_path, file_list, dir_list):
+        """
+        遍历指定文件夹获取每一个文件路径
+        :param root_path: 根目录
+        :param file_list: 需保存的文件列表
+        :param dir_list: 目录列表
+        :return: 文件列表
+        """
+        file_or_dir = os.listdir(root_path)
+        for file_dir in file_or_dir:
+            file_dir_path = os.path.join(root_path, file_dir)
+            if os.path.isdir(file_dir_path):
+                dir_list.append(file_dir_path)
+                self.walk_dir(file_dir_path, file_list, dir_list)
+            else:
+                file_list.append(file_dir_path)
+
+        return file_list
+
+        pass
+
+    def get_video_info(self, file):
+        """
+        获取视频信息：fps,width,height,file_name
+        :param file:视频文件
+        :return:
+        """
+        file_type = file.split('.')[-1]
+        if file_type == 'mp4' or file_type == 'MP4' or file_type == 'avi' or file_type == 'AVI':
+            cap = cv2.VideoCapture(file)
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            file_name = file.split('.')[0]
+            fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+
+            print(fps, width, height, file_name, fourcc)
+            return [fps, width, height, file_name, fourcc, file_type]
+        else:
+            return []
+
+    def create_img(self, width, height, text='title', logo_text='logo', color=(100, 100, 100, 100)):
+        """
+        创建背景图片
+        :param width:视频宽度
+        :param height:视频高度
+        :param text:视频标题
+        :param logo_text:logo文字
+        :param color:
+        :return:
+        """
+        # old_new_img = Image.new('RGBA', (int(width), int(height)), color)
+        origin_img = Image.open('background.png')
+        new_img = origin_img.crop((0, 0, int(width), int(height)))
+        self.draw_img(new_img, text, logo_text)
+        new_img = self.add_img_logo(new_img, 'icon.png')
+        new_img.save(f'{text}.png')
+        pass
+
+    def add_img_logo(self, new_img, icon):
+        """
+        粘贴logo图标
+        :param new_img: 背景图片
+        :param icon: logo
+        :return:
+        """
+        # img_size = new_img.size
+        logo_img = Image.open(icon)
+        new_img.paste(logo_img, (40, 40))
+        return new_img
+
+        pass
+
     def draw_img(self, new_img, title_text, logo_text):
         """
         将文字放到图片上
@@ -51,71 +137,6 @@ class Videos:
 
         pass
 
-    def add_img_logo(self, new_img, icon):
-        """
-        粘贴logo图标
-        :param new_img: 背景图片
-        :param icon: logo
-        :return:
-        """
-        # img_size = new_img.size
-        logo_img = Image.open(icon)
-        new_img.paste(logo_img, (40, 40))
-        return new_img
-
-        pass
-
-    def create_img(self, width, height, text='title', logo_text='logo', color=(100, 100, 100, 100)):
-        """
-        创建背景图片
-        :param width:视频宽度
-        :param height:视频高度
-        :param text:视频标题
-        :param logo_text:logo文字
-        :param color:
-        :return:
-        """
-        # old_new_img = Image.new('RGBA', (int(width), int(height)), color)
-        origin_img = Image.open('background.png')
-        new_img = origin_img.crop((0, 0, int(width), int(height)))
-        self.draw_img(new_img, text, logo_text)
-        new_img = self.add_img_logo(new_img, 'icon.png')
-        new_img.save(f'{text}.png')
-        pass
-
-    def get_video_info(self, file):
-        """
-        获取视频信息：fps,width,height,file_name
-        :param file:视频文件
-        :return:
-        """
-        file_type = file.split('.')[-1]
-        if file_type == 'mp4' or file_type == 'MP4' or file_type == 'avi' or file_type == 'AVI':
-            cap = cv2.VideoCapture(file)
-            fps = int(cap.get(cv2.CAP_PROP_FPS))
-            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            file_name = file.split('.')[0]
-            fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
-
-            print(fps, width, height, file_name, fourcc)
-            return [fps, width, height, file_name, fourcc, file_type]
-        else:
-            return []
-
-    def gen_file(self, path):
-        """
-        读取文件
-        :param path:
-        :return:
-        """
-        files_list = os.listdir(path)
-        file_list = []
-        for file in files_list:
-            if not os.path.isdir(file):
-                file_list.append(file)
-        return file_list
-
     def create_video(self, videos_name, fourcc, fps, resolution, videos_type):
         """
         创建视频
@@ -151,7 +172,8 @@ class Videos:
 if __name__ == '__main__':
     v = Videos()
     # v.create_img(100, 200, text='这是测试')
-    files = v.gen_file('./')
+    # files = v.gen_file('./')
+    files = v.walk_dir("./", [], [])
     for file in files:
         video_info = v.get_video_info(file)
         if video_info != []:
